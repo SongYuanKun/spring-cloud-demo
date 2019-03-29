eec10d5aaabfcfd485d07c3c2557ccef99160f29
123839070@qq.com <kun3.1415926>
2019-03-29 18:53:27 +0800

    增加config中心服务端

---
+++ config-server/src/main/java/com/songyuankun/cloud/config/server/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.songyuankun.cloud.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * cloud-config的服务端
+ *
+ * @author Administrator
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
