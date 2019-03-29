cedaf368ec16626218da4a5334892f15472d8cd2
123839070@qq.com <kun3.1415926>
2019-03-29 16:13:52 +0800

    Initial commit

---
+++ eureka-server/src/main/java/com/songyuankun/cloud/eureka/server/EurekaServiceApplication.java
@@ -0,0 +1,20 @@
+package com.songyuankun.cloud.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eureka服务端
+ *
+ * @author Administrator
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceApplication.class, args);
+    }
+
+}
