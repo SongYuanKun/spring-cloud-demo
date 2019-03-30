81729dc99f73a313c62ab2acf0c9c618d989007c
123839070@qq.com <kun3.1415926>
2019-03-30 16:31:47 +0800

    config-server增加了eureka client

--- eureka-app/src/main/java/com/songyuankun/cloud/eureka/app/EurekaAppApplication.java
+++ eureka-app/src/main/java/com/songyuankun/cloud/eureka/app/EurekaAppApplication.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * eureka客户端
+ *
+ * @author Administrator
+ */
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
