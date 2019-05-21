74f9f2899087a06b2367ea1f0de676c677711f14
songyuankun <songyuankun@huohua.cn>
2019-05-21 16:26:46 +0800

    响应对象

--- common/src/main/java/com/songyuankun/cloud/common/ResponseUtils.java
+++ common/src/main/java/com/songyuankun/cloud/common/ResponseUtils.java
@@ -6,7 +6,7 @@ package com.songyuankun.cloud.common;
 public class ResponseUtils {
 
     public static <T> Response<T> success(T object) {
-        return new Response<T>(ResultEnums.SUCCESS.getCode(), ResultEnums.SUCCESS.getMessage(), object);
+        return new Response<>(ResultEnums.SUCCESS.getCode(), ResultEnums.SUCCESS.getMessage(), object);
     }
 
 }
