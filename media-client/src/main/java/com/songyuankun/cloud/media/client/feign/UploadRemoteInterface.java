f9021bafc2f97980ab92fe2125f9fc67e9eefc25
songyuankun <songyuankun@huohua.cn>
2019-05-23 16:44:19 +0800

    上传文件不好用，未修复

--- media-client/src/main/java/com/songyuankun/cloud/media/client/feign/UploadRemoteInterface.java
+++ media-client/src/main/java/com/songyuankun/cloud/media/client/feign/UploadRemoteInterface.java
@@ -2,9 +2,14 @@ package com.songyuankun.cloud.media.client.feign;
 
 
 import com.songyuankun.cloud.common.Response;
+import feign.form.spring.SpringFormEncoder;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -12,7 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
  * @author songyuankun
  */
 @Component
-@FeignClient(name = "UPLOAD", fallback = UploadRemoteFallback.class)
+@FeignClient(name = "UPLOAD", fallback = UploadRemoteFallback.class, configuration = UploadRemoteInterface.MultipartSupportConfig.class)
 public interface UploadRemoteInterface {
     /**
      * 上传文件
@@ -20,6 +25,14 @@ public interface UploadRemoteInterface {
      * @param file 文件
      * @return key
      */
-    @PostMapping(value = "upload/file")
+    @RequestMapping(method = RequestMethod.POST, value = "upload/file", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     Response<String> uploadFile(@RequestParam("file") MultipartFile file);
+
+    @Configuration
+    class MultipartSupportConfig {
+        @Bean
+        public SpringFormEncoder feignFormEncoder() {
+            return new SpringFormEncoder();
+        }
+    }
 }
