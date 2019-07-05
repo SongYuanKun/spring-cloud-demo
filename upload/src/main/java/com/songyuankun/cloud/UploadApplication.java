94f2636501fd07c085f7f473e0c48271fe6bf33e
songyuankun <songyuankun@huohua.cn>
2019-07-05 15:40:31 +0800

    增加文件上传大小

--- upload/src/main/java/com/songyuankun/cloud/UploadApplication.java
+++ upload/src/main/java/com/songyuankun/cloud/UploadApplication.java
@@ -2,17 +2,34 @@ package com.songyuankun.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+
+import javax.servlet.MultipartConfigElement;
 
 /**
  * @author sonyuankun
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@Configuration
 public class UploadApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UploadApplication.class, args);
     }
 
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //单个文件最大
+        factory.setMaxFileSize( DataSize.ofBytes(1024024024));
+        /// 设置总上传数据总大小
+        factory.setMaxRequestSize( DataSize.ofBytes(1024024024));
+        return factory.createMultipartConfig();
+    }
 }
